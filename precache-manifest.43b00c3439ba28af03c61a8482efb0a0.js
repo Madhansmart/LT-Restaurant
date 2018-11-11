2bbeac29fd8b4f6725d3608aaee5a3c6ec8173ff
MADHAN <madhansmart7404@gmail.com>
2018-11-11 10:06:16 +0530

    Updates

---
+++ precache-manifest.43b00c3439ba28af03c61a8482efb0a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "de14dd4dec03c6e50e48517a3fff45ef",
+    "url": "/LT-Restaurant/static/media/main.de14dd4d.jpg"
+  },
+  {
+    "revision": "66c87204f3d7002f1763bdb7153050bb",
+    "url": "/LT-Restaurant/static/media/boxTwo.66c87204.jpg"
+  },
+  {
+    "revision": "b730ecdf00944af1f07276946aa478bb",
+    "url": "/LT-Restaurant/static/media/boxThree.b730ecdf.jpg"
+  },
+  {
+    "revision": "066b5f4eeaa1a066358494217fe2d3f8",
+    "url": "/LT-Restaurant/static/media/6.066b5f4e.jpg"
+  },
+  {
+    "revision": "578ff132ec1c00ebc918",
+    "url": "/LT-Restaurant/static/js/runtime~main.578ff132.js"
+  },
+  {
+    "revision": "c07385d99ee4ade0f519",
+    "url": "/LT-Restaurant/static/js/main.c07385d9.chunk.js"
+  },
+  {
+    "revision": "1fbdece56fd080003e45",
+    "url": "/LT-Restaurant/static/js/1.1fbdece5.chunk.js"
+  },
+  {
+    "revision": "c07385d99ee4ade0f519",
+    "url": "/LT-Restaurant/static/css/main.76f95bf9.chunk.css"
+  },
+  {
+    "revision": "1fbdece56fd080003e45",
+    "url": "/LT-Restaurant/static/css/1.bdcb22ba.chunk.css"
+  },
+  {
+    "revision": "adc6814700db6a03e119d04b2f75901d",
+    "url": "/LT-Restaurant/index.html"
+  }
+];
